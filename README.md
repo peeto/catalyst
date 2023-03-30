601e1093b78c991179b75caf0d958d4fe2332a23
peeto <chris@peeto.net>
2023-03-30 13:56:49 +1100

    Added code and comments for the Logic Test task.

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Catalyst PHP tasks
 
 Repository for work on Catalyst PHP competency test.
+
+## 2. Logic test
+
+This was done as a single commit because the separator generation and modulo functions were thought out before hand. A class was used to show the breakdown in logic to achieve the required outcome. One issue with my approach is it makes it difficult to modify the min (1) and max (100) requirements as the separator generator would need to be aware of the minimum value. This I would solve by instantaing the class and storing the min and max as properties.
+
+To run this from the repository root:
+
+```
+php -f src/foorbar.php
+```
+
---
+++ src/foobar.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace catalyst\foobar;
+
+class FooBar
+{
+	protected function getSeperator(int $n): string {
+		// Separate the iterations correctly
+		return $n > 1 ? ', ' : '';
+	}
+
+	protected function getIteratorString(int $n): string {
+		$output = '';
+		
+		// The next two lines are carfully written to be able to produce foo, bar, or foobar
+		if ($n % 3 == 0) $output .= 'foo';
+		if ($n % 5 == 0) $output .= 'bar';
+
+		// If not foo, bar, or foobar use $n
+		if ($output == '') $output = (string)$n;
+
+		return self::getSeperator($n) . $output;
+	}
+
+	protected function iterate(): void {
+		for ($n = 1; $n <= 100; $n++) {
+			echo self::getIteratorString($n);
+		}
+	}
+
+	public function init(): void {
+		self::iterate();
+
+	}
+}
+
+FooBar::init();
+
