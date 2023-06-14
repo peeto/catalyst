6f4a80bdeec766c0c35030d5debf17835c6adb35
peeto <chris@peeto.net>
2023-06-14 15:13:42 +1000

    Add newlines as per feedback. Make start and finish configurable. Ensure start isn't after finish.

--- src/foobar.php
+++ src/foobar.php
@@ -4,35 +4,36 @@ namespace catalyst\foobar;
 
 class FooBar
 {
-	protected static function getSeperator(int $n): string {
-		// Separate the iterations correctly
-		return $n > 1 ? ', ' : '';
-	}
+    protected static function getIteratorString(int $n): string {
+        $output = '';
 
-	protected static function getIteratorString(int $n): string {
-		$output = '';
-		
-		// The next two lines are carfully written to be able to produce foo, bar, or foobar
-		if ($n % 3 == 0) $output .= 'foo';
-		if ($n % 5 == 0) $output .= 'bar';
+        // The next two lines are carfully written to be able to produce foo, bar, or foobar
+        if ($n % 3 == 0) $output .= 'foo';
+        if ($n % 5 == 0) $output .= 'bar';
 
-		// If not foo, bar, or foobar use $n
-		if ($output == '') $output = (string)$n;
+        // If not foo, bar, or foobar use $n
+        if ($output == '') $output = (string)$n;
 
-		return self::getSeperator($n) . $output;
-	}
+        return $output;
+    }
 
-	protected static function iterate(): void {
-		for ($n = 1; $n <= 100; $n++) {
-			echo self::getIteratorString($n);
-		}
-	}
+    public static function iterate(int $start, int $finish): void {
+        if ($start > $finish) {
+            echo "\$finish($finish) can't be smaller than \$start($start)\r\n\r\n";
+            return;
+        }
+        
+        for ($n = $start; $n <= $finish; $n++) {
+            echo ($n > $start ? ', ' : '') . self::getIteratorString($n);
+        }
+        echo "\r\n\r\n";
+        return;
+    }
 
-	public static function init(): void {
-		self::iterate();
+    public static function init(): void {
+        self::iterate(1, 100);
 
-	}
+    }
 }
-
 FooBar::init();
 
