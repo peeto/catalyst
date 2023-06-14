58c352ae4ce51474f8675750766acaeba37251c2
peeto <chris@peeto.net>
2023-06-14 17:33:50 +1000

    Fix whitepace. Add primary "id" column. Add considerable exception catching. Check MIME types. Count errors. Use prepared insert statements. Encode database name. Use variables for CSV data.

--- src/user_upload.php
+++ src/user_upload.php
@@ -4,172 +4,273 @@ namespace catalyst\userupload;
 
 class UserUpload
 {
-	protected static function createTable($db): void {
-		$sql = "CREATE TABLE IF NOT EXISTS `users` (
-		  `name` VARCHAR(1024) NOT NULL,
-		  `surname` VARCHAR(1024) NOT NULL,
-		  `email` VARCHAR(1024) NOT NULL,
-		  UNIQUE INDEX `email` (`email` ASC) VISIBLE,
-		  INDEX `name` (`name` ASC) VISIBLE,
-		  INDEX `surname` (`surname` ASC) VISIBLE);";
-		
-		if (!$db->query($sql)) {
-			echo "\r\nError: " . $db->error. "\r\n\r\n";
-		} else {
-			echo "\r\nTable `users` created or exists.\r\n\r\n";
-		}
-	}
+    protected static function createTable($db): void {
+        $sql = "CREATE TABLE `users` (
+            `id` int unsigned NOT NULL AUTO_INCREMENT,
+            `name` varchar(255) NOT NULL,
+            `surname` varchar(255) NOT NULL,
+            `email` varchar(320) NOT NULL,
+            PRIMARY KEY (`id`),
+            UNIQUE KEY `id_UNIQUE` (`id`),
+            UNIQUE KEY `email` (`email`),
+            KEY `name` (`name`),
+            KEY `surname` (`surname`)
+          ) ENGINE=InnoDB;
+        ";
 
-	protected static function parseCSVFile(string $file): array {
-		$results = [];
-		if (($handle = fopen($file, "r")) !== FALSE) {
-			$ignoreHeader = true;
-			while (($data = fgetcsv($handle, 1024, ",")) !== FALSE) {
-				if ($ignoreHeader) {
-					$ignoreHeader = false;
-				} else  {
-					$results[] = $data;
-				}
-			}
-		}
-		return $results;
-	}
+        if (!$db->query($sql)) {
+            echo "\r\nError: " . $db->error. "\r\n\r\n";
+        } else {
+            echo "\r\nTable `users` created or exists.\r\n\r\n";
+        }
+    }
+    
+    protected static function isCSVFile(string $file): bool {
+        $csvTypes = array(
+            'text/x-comma-separated-values',
+            'text/comma-separated-values',
+            'application/octet-stream',
+            'application/vnd.ms-excel',
+            'application/x-csv',
+            'text/x-csv',
+            'text/csv',
+            'application/csv',
+            'application/excel',
+            'application/vnd.msexcel',
+            'text/plain'
+            );
+        $isCSV = false;
+        
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mime = finfo_file($finfo, $file);
+        $isCSV =  in_array($mime, $csvTypes) === true;
+        finfo_close($finfo);
+        
+        return $isCSV;
+    }
 
-	protected static function parseName(string $name): string {
-		return trim(ucfirst(strtolower($name)));
-	}
+    protected static function parseCSVFile(string $file): array {
+        if (!file_exists($file)) {
+            echo "\r\nFile $file does not exist.\r\n\r\n";
+            exit();
+        }
+        if (!self::isCSVFile($file)) {
+            echo "\r\nFile $file is not of type CSV.\r\n\r\n";
+            exit();
+        }
+        
+        $results = [];
+        try {
+            if (($handle = fopen($file, "r")) !== FALSE) {
+                $ignoreHeader = true;
+                while (($data = fgetcsv($handle, 1024, ",")) !== FALSE) {
+                    if ($ignoreHeader) {
+                        $ignoreHeader = false;
+                    } else  {
+                        $results[] = $data;
+                    }
+                }
+            }
+        } catch (Exception $ex) {
+            echo "Could not parse CSV in $file. Possible wrong file, record structure or encoding.\r\n";
+            exit();
+        }
+        return $results;
+    }
 
-	protected static function checkEmail(string $email): bool {
-		return !!filter_var(trim($email), FILTER_VALIDATE_EMAIL);
-	}
+    protected static function parseName(string $name): string {
+        return trim(ucfirst(strtolower($name)));
+    }
 
-	protected static function parseEmail(string $email): string {
-		return trim(strtolower($email));
-	}
+    protected static function checkEmail(string $email): bool {
+        return !!filter_var(trim($email), FILTER_VALIDATE_EMAIL);
+    }
 
-	protected static function dryRun(string $file): void {
-		$data = self::parseCSVFile($file);
+    protected static function parseEmail(string $email): string {
+        return trim(strtolower($email));
+    }
 
-		echo "\r\n". count($data) . " records found\r\n\r\n";
+    protected static function dryRun(string $file): void {
+        $data = self::parseCSVFile($file);
 
-		foreach ($data as $r) {
-			echo "Firstname: " . self::parseName($r[0]);
-			echo " Surname: " . self::parseName($r[1]);
-			if (!self::checkEmail($r[2])) {
-				echo " Invalid email address found!";
-			}
-			echo " Email: " . self::parseEmail($r[2]);;
-			echo "\r\n";
-		}
-		echo "\r\n";
-	}
+        echo "\r\n". count($data) . " records found.\r\n\r\n";
+        $errors = 0;
 
-	protected static function dbUpload($db, string $file): void {
+        try {
+            $record = 1;
+            foreach ($data as $r) {
+                @list($name, $surname, $email) = $r;
+                if ($name == '' || $surname == '' || $email == '') {
+                    echo "Empty required data found on record $record.\r\n";
+                    $errors++;
+                    continue;
+                }
+                $name = self::parseName($name);
+                $surname = self::parseName($surname);
+                $email = self::parseEmail($email);
+                
+                echo "Firstname: $name";
+                echo " Surname: $surname";
+                if (!self::checkEmail($r[2])) {
+                    echo " Invalid email address found!";
+                    $errors++;
+                }
+                echo " Email: $email";
+                echo "\r\n";
+                $record++;
+            }
+            echo "\r\n";
+        } catch (Exception $ex) {
+            echo "General decoding error. Is CSV file $file correct?\r\n\r\n";
+            exit();
+        }
+        echo "\r\n". $errors . " errors found.\r\n\r\n";
+        
+    }
 
-		$data = self::parseCSVFile($file);
+    protected static function dbUpload($db, string $file): void {
 
-		echo "\r\n". count($data) . " records found\r\n\r\n";
+        $data = self::parseCSVFile($file);
 
-		foreach ($data as $r) {
-			if (!self::checkEmail($r[2])) {
-				echo "Invalid email address found: " . self::parseEmail($r[2]) . "\r\n";
-			} else {
-				$sql = "INSERT INTO users (name, surname, email) VALUES (";
-				$sql .= "'" . $db->real_escape_string(self::parseName($r[0])) . "'";
-				$sql .= ", '" . $db->real_escape_string(self::parseName($r[1])) . "'";
-				$sql .= ", '" . $db->real_escape_string(self::parseEmail($r[2])) . "'";
-				$sql .= ");";
-				$db->query($sql);
-			}
-		}
-	}
+        echo "\r\n". count($data) . " records found.\r\n\r\n";
+        $errors = 0;
+        
+        try {
+            $record = 1;
+            foreach ($data as $r) {
+                @list($name, $surname, $email) = $r;
+                if ($name == '' || $surname == '' || $email == '') {
+                    echo "Empty required data found on record $record.\r\n";
+                    $errors++;
+                    continue;
+                }
 
-	protected static function parseCommand(): void {
-		$opts = getopt('u:p:h:d:', [
-			'file:',
-			'create_table',
-			'dry_run',
-			'help'
-		]);
+                if (!self::checkEmail($email)) {
+                    $errors++;
+                    echo "Invalid email address found: " . self::parseEmail($email) . "\r\n";
+                } else {
+                    $name = self::parseName($name);
+                    $surname = self::parseName($surname);
+                    $email = self::parseEmail($email);
 
-		if (isset($opts['help'])) {
-			self::writeHelp();
-			return;
-		}
+                    try {
+                        $sql = "INSERT INTO users (name, surname, email) VALUES (?, ?, ?);";
+                        $stmt = $db->prepare($sql);
+                        $stmt->bind_param("sss", $name, $surname, $email);
+                        $stmt->execute();
+                        if (!$stmt->insert_id) {
+                            $errors++;
+                            echo "Could not insert record for $email. Possible dupliate?\r\n";
+                        }
+                    } catch (Exception $ex) {
+                        $errors++;
+                        echo "Could not insert record for $email. Possible dupliate?\r\n";
+                    }
+                }
+                $record++;
+            }
+        } catch (Exception $ex) {
+            echo "General decoding error. Is CSV file $file correct?\r\n\r\n";
+            exit();
+        }
+        echo "\r\n". $errors . " errors found.\r\n\r\n";
+    }
 
-		if (isset($opts['dry_run']) && !isset($opts['file'])) {
-			echo "\r\n'dry_run' requires 'file' to be specified. Consult help for more information.\r\n\r\n";
-			return;
-		}
+    protected static function parseCommand(): void {
+        $opts = getopt('u:p:h:d:', [
+            'file:',
+            'create_table',
+            'dry_run',
+            'help'
+        ]);
 
-		if (isset($opts['dry_run']) && isset($opts['file'])) {
-			self::dryRun($opts['file']);
-			return;
-		}
+        if (isset($opts['help'])) {
+            self::writeHelp();
+            return;
+        }
 
-		$hasDbOpts = isset($opts['u'])
-			&& isset($opts['p'])
-			&& isset($opts['d'])
-			&& isset($opts['h']);
+        if (isset($opts['dry_run']) && !isset($opts['file'])) {
+            echo "\r\n'dry_run' requires 'file' to be specified. Consult help for more information.\r\n\r\n";
+            return;
+        }
 
-		if (isset($opts['create_table']) || isset($opts['file'])) {
-			if (!$hasDbOpts) {
-				echo "\r\nAll MySQL options must be provided. Consult help for more information.\r\n\r\n";
-				return;
-			}
+        if (isset($opts['dry_run']) && isset($opts['file'])) {
+            self::dryRun($opts['file']);
+            return;
+        }
 
-			$db = new \mysqli(
-				$opts['h'],
-				$opts['u'],
-				$opts['p'],
-				$opts['d']);
-			$db->query('USE "' . $opts['d'] . '";');
+        $hasDbOpts = isset($opts['u'])
+            && isset($opts['p'])
+            && isset($opts['d'])
+            && isset($opts['h']);
 
-			if (isset($opts['create_table'])) {
-				self::createTable($db);
-				return;
-			}
+        if (isset($opts['create_table']) || isset($opts['file'])) {
+            if (!$hasDbOpts) {
+                echo "\r\nAll MySQL options must be provided. Consult help for more information.\r\n\r\n";
+                return;
+            }
 
-			if (isset($opts['file'])) {
-				self::dbUpload($db, $opts['file']);
-				return;
-			}
-		}
+            try {
+                $db = new \mysqli(
+                    $opts['h'],
+                    $opts['u'],
+                    $opts['p'],
+                    $opts['d']);
+                if ($db->connect_errno) {
+                    echo "Failed to connect to MySQL: " . $db->connect_error . "\r\n\r\n";
+                    exit();
+                }
+                $db->query('USE "' . $db->real_escape_string($opts['d']) . '";');
+            } catch (Exception $ex) {
+                echo "Could not connect to database, check parameters and try again.\r\n";
+                die();
+            }
 
-		echo "\r\nCommand not understood\r\n";
-		self::writeHelp();
-	}
+            if (isset($opts['create_table'])) {
+                self::createTable($db);
+                return;
+            }
 
-	protected static function writeHelp(): void {
-		echo "\r\nphp -f src/user_upload.php --\r\n";
-		echo "\r\nAvailable options are:\r\n";
-		echo <<< EOT
+            if (isset($opts['file'])) {
+                self::dbUpload($db, $opts['file']);
+                return;
+            }
+        }
 
-			--file [csv file name] – this is the name of the CSV to be parsed
+        echo "\r\nCommand not understood.\r\n";
+        self::writeHelp();
+    }
 
-			--create_table – this will cause the MySQL users table to be built (and no further
-				action will be taken)
+    protected static function writeHelp(): void {
+        echo "\r\nphp -f src/user_upload.php --\r\n";
+        echo "\r\nAvailable options are:\r\n";
+        echo <<< EOT
 
-			--dry_run – this will be used with the --file directive in case we want to run the script but not
-				insert into the DB. All other functions will be executed, but the database wont be altered
+            --file [csv file name] – this is the name of the CSV to be parsed
 
-			-u – MySQL username
+            --create_table – this will cause the MySQL users table to be built (and no further
+                    action will be taken)
 
-			-p – MySQL password
+            --dry_run – this will be used with the --file directive in case we want to run the script but not
+                    insert into the DB. All other functions will be executed, but the database wont be altered
 
-			-d – MySQL database name
+            -u – MySQL username
 
-			-h – MySQL host
+            -p – MySQL password
 
-			--help – which will output the above list of directives with details.
+            -d – MySQL database name
+
+            -h – MySQL host
+
+            --help – which will output the above list of directives with details.
 
 
-		EOT;
-	}
+        EOT;
+    }
 
-	public static function init(): void {
-		self::parseCommand();
-	}
+    public static function init(): void {
+        self::parseCommand();
+    }
 }
 
 UserUpload::init();
